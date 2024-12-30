cd904de85d471da67441301ca378ea2a9a720776
Nova <Mahdi.zrei@gmail.com>
2024-12-31 00:24:39 +0330

    remove windows 7 build

--- script/build_go.sh
+++ script/build_go.sh
@@ -9,10 +9,6 @@ source script/env_deploy.sh
 [ "$GOOS" == "linux" ] && [ "$GOARCH" == "arm64" ] && DEST=$DEPLOYMENT/linux-arm64 || true
 [ "$GOOS" == "darwin" ] && [ "$GOARCH" == "amd64" ] && DEST=$DEPLOYMENT/macos-amd64 || true
 [ "$GOOS" == "darwin" ] && [ "$GOARCH" == "arm64" ] && DEST=$DEPLOYMENT/macos-arm64 || true
-if [ $GOOS = "windows7" ]; then
-  GOOS=windows
-  OLD=y
-fi
 
 if [ -z $DEST ]; then
   echo "Please set GOOS GOARCH"
@@ -30,9 +26,5 @@ popd
 
 #### Go: nekobox_core ####
 pushd core/cmd/nekobox_core
-if [ -z $OLD ]; then
-  go build -v -o $DEST -trimpath -ldflags "-w -s -X $neko_common.Version_neko=$version_standalone" -tags "with_clash_api,with_gvisor,with_quic,with_wireguard,with_utls,with_ech,with_dhcp"
-else
-  go build -v -o $DEST -trimpath -ldflags "-w -s -X $neko_common.Version_neko=$version_standalone" -tags "with_clash_api,with_gvisor,with_quic,with_wireguard,with_utls,with_dhcp"
-fi
+go build -v -o $DEST -trimpath -ldflags "-w -s -X $neko_common.Version_neko=$version_standalone" -tags "with_clash_api,with_gvisor,with_quic,with_wireguard,with_utls,with_ech,with_dhcp"
 popd
